72dd26624b67f8626171a8bfe8a8b0c9b0847c57
Hanush H Nair <hanushh@gmail.com>
2023-04-25 13:17:50 +0530

    dynamic article

--- blocks/dynamic-articles/dynamic-articles.js
+++ blocks/dynamic-articles/dynamic-articles.js
@@ -5,11 +5,9 @@ export default async function decorate(block) {
     const ulEl = document.createElement("ul");
     json.data.forEach(element => {
         console.log(element);
-        if (element.path.includes("/magazine")) {
             const liEl = document.createElement("li");
             liEl.innerText = element.title;
             ulEl.appendChild(liEl);
-        }
 
     });
     block.appendChild(ulEl);
